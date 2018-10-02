e7e8c3dca73e0087c84db9f342f80b6a01f7419e
Prateek Sachan <ps@prateeksachan.com>
2018-10-02 14:09:23 +0530

    remove use of async def

--- elasticsearch_async/connection_pool.py
+++ elasticsearch_async/connection_pool.py
@@ -8,8 +8,9 @@ class AsyncConnectionPool(ConnectionPool):
         self.loop = loop
         super().__init__(connections, **kwargs)
 
-    async def close(self):
-        await asyncio.wait([conn.close() for conn in self.orig_connections],
+    @asyncio.coroutine
+    def close(self):
+        await ([conn.close() for conn in self.orig_connections],
                            loop=self.loop)
 
 
